a2034abafcc3a7004bce34f7ab11d7f178a3bcba
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2023-09-14 15:17:40 +0200

    update 'semestr2'

---
+++ Semestr 2/Programowanie z wykładów sem. 2/Wykład 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdalign>
+
+using namespace std;
+
+
+struct Pracownik
+{
+    string imie;
+    string nazwisko;
+    double placa;
+};
+
+void wpisz(Pracownik P)
+{
+
+}
+
+void wyswietl(Pracownik p)
+{
+
+}
+
+int main ()
+{
+    cout << "xyz" << endl;
+}
