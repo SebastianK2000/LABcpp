7ec1b088c1c16f9b9f5d5115abe4cc24bbf2b9fb
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2023-09-16 00:21:57 +0200

    update

--- Semestr 2/Kod z wykładów/Wykład 3 cz.2.cpp
+++ Semestr 2/Kod z wykładów/Wykład 3 cz.2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Osoba
 {
-private:
+protected:
     string imie;
     string nazwisko;
 public:
@@ -27,7 +27,7 @@ public:
 class Pracownik: public Osoba
 {
 
-private:
+protected:
     string stanowisko;
 
 public:
@@ -41,12 +41,44 @@ public:
         Osoba::wyswietl();
         cout<<endl<<"Stanowisko: "<<stanowisko;
     }
+
+    void wyswietlNazwiskoIStanowisko()
+    {
+        cout<<endl<<"Nazwisko: "<<nazwisko;
+        cout<<endl<<"Stanowisko: "<<stanowisko;
+    }
+};
+
+class PracownikB2B: public Pracownik
+{
+protected:
+    string typUmowy;
+
+public:
+
+    PracownikB2B(string imie, string nazwisko, string stanowisko, string typUmowy)
+    : Pracownik(imie, nazwisko, stanowisko), typUmowy(typUmowy)
+    {
+
+    }
+    void wyswietl()
+    {
+        Pracownik::wyswietl();
+        cout<<endl<<"Typ umowy to: "<<typUmowy;
+    }
+
 };
 
 int main(int argc, char* argv[])
 {
+    Osoba o1("Adam", "Rak");
+    o1.wyswietl();
+
     Pracownik p1("John", "Doe", "Intern");
     p1.wyswietl();
+
+    PracownikB2B p2 ("John", "Silwerheand", "Junior", "B2B");
+    p2.wyswietl();
 
     return 0;
 }
